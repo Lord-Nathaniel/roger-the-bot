5cf6e56405280dff300b69db5f3c9797233acc0e
Adrien Lozano <ad_lozano@outlook.com>
2021-05-23 01:10:04 +0200

    :construction_worker: module_emoji : added verification from message / added args from input from users / added emoji display function from input

--- module_emoji.js
+++ module_emoji.js
@@ -12,8 +12,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const BOT_PREFIX = "$";
-const BOOSTER_COMMAND = "flute-me";
-const BOOSTER_TABLE = ["\u{1F973}", "\u{1F60D}", "\u{1F30D}", "\u{1F359}", "\u{1F0CF}"];
+const BOOSTER_COMMAND = "booster";
+const BOOSTER_TABLE = ["\u{1F973}", "\u{1F60D}", "\u{1F30D}", "\u{1F359}", "\u{1F0CF}", "\u{1F355}", "\u{1F3B2}", "\u{1F404}", "\u{1F451}", "\u{1F44C}"];
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -23,24 +23,34 @@ client.on('ready', () => {
     console.log("Gisèle à lancé le module emoji");
 });
 
-// client.on("message", message => {
-//     //on send $booster, give 5 random emoji
-//     if (message.content === `${ BOT_PREFIX }${ BOOSTER_COMMAND }`) {
-//         let number = 5;
-//         let rand = getRandom(number);
-//         for (let i = 0; i < number; i++) {
-//             message.react(BOOSTER_ABLE[rand[i]]);
-//         }
-//     }
-// });
+client.on("message", message => {
+    //exit the function if the message doesn't start with the prefix or is from the bot
+    if (!message.content.startsWith(BOT_PREFIX) || message.author.bot) return;
 
-// function getRandom(number) {
-//     let tempNumb = [];
-//     for (let i = 0; i < number; i++) {
-//         tempNumb.push(Math.random() * BOOSTER_TABLE.length());
-//     }
-//     return tempNumb;
-// }
+    //create an args and command to take input from user
+    const args = message.content.slice((BOT_PREFIX).length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    //take a number in first argument, and display this number of emojis
+    if (command === "booster") {
+        if (!args.length) {
+            return message.channel.send("Veuillez entrer un nombre de cartes à piocher !");
+        }
+        let number = args[0];
+        let emoji = getRandomEmoji(number);
+        message.channel.send(emoji);
+    }
+});
+
+function getRandomEmoji(number) {
+    let tempEmoji = [];
+    for (let i = 0; i < number; i++) {
+        let rand = Math.round(Math.random() * (BOOSTER_TABLE.length - 1));
+        tempEmoji.push(BOOSTER_TABLE[rand]);
+        console.log(rand);
+    }
+    return tempEmoji;
+}
 
 // Log our bot in using the token from https://discord.com/developers/applications
 client.login(process.env.BOT_TOKEN)
