566cd86d4448015dd7809ab4b9710e62f58f9778
Adrien Lozano <ad_lozano@outlook.com>
2021-06-06 21:58:08 +0200

    :card_file_box: displaced module_ping in module_basics

--- modules/module_basics.js
+++ modules/module_basics.js
@@ -2,10 +2,12 @@ const command = require('../command');
 
 module.exports = (client) => {
 
+    //a basic $ping command
     command(client, ['ping', 'test'], (message) => {
         message.channel.send('ewo !');
     });
 
+    //a command to get the number of member in the channel
     command(client, 'servers', message => {
         client.guilds.cache.forEach((guild) => {
             message.channel.send(
@@ -14,6 +16,7 @@ module.exports = (client) => {
         });
     });
 
+    //a command to delete message in a channel
     command(client, ['cc', 'clearchannel'], message => {
         if (message.member.hasPermission('ADMINISTRATOR')) {
             message.channel.messages.fetch().then((results) => {
@@ -22,6 +25,7 @@ module.exports = (client) => {
         }
     });
 
+    //a command to set the status of the bot
     command(client, 'status', message => {
         const content = message.content.replace('$status', '');
 
@@ -31,6 +35,25 @@ module.exports = (client) => {
                 type: 0
             }
         })
+    });
+
+    //a command to give an heart emoji to the message listened
+    command(client, 'j\'aime les castors', message => {
+        message.react("\u{1F499}");
+    });
+
+    //give the "flutted" role
+    command(client, 'fluteMe', message => {
+        fluteUser(message.member);
+    });
+
+    //React to message deletion
+    client.on("messageDelete", message => {
+        message.reply("Je t'ai vu supprimer ce message ( ͡° ͜ʖ ͡°)");
     })
+
+    function fluteUser(member) {
+        member.roles.add("845764114814337061");
+    }
 
 }
